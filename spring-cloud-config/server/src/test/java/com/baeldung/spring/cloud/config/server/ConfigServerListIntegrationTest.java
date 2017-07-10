92e0e6a0b095e144674fc724f516343c6e4b2287
shagaba <shai.gabai@shagaba.com>
2017-07-10 14:29:10 +0300

    start spring cloud config

---
+++ spring-cloud-config/server/src/test/java/com/baeldung/spring/cloud/config/server/ConfigServerListIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.baeldung.spring.cloud.config.server;
+
+/*
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigServer.class)
+@WebAppConfiguration
+@Ignore
+public class ConfigServerListIntegrationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
+*/
